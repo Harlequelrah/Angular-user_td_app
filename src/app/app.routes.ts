7d45a9242e47ec1b63905c2d1d7fbe965da5d993
Harlequelrah <degbovimax19@gmail.com>
2025-01-29 18:05:11 +0000

    frontend de create-user , user-list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { CreateUserComponent } from './create-user/create-user.component';
+import { UserListComponent } from './user-list/user-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'create-user',
+        component:CreateUserComponent
+    },
+    {
+        path: 'list-user',
+        component:UserListComponent
+    }
+];
--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user-list',
+  standalone: true,
   imports: [],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.scss'
